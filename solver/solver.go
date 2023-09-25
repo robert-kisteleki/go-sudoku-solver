a28dd5e4f368af3eca943d103a30d4dde72c0005
Robert Kisteleki <kistel@kistel.eu>
2023-09-25 21:13:19 +0200

    strategy 2 and tests

--- solver/solver.go
+++ solver/solver.go
@@ -10,6 +10,7 @@ import (
 type Sudoku struct {
 	matrix     [9][9]int // the (working) table
 	complexity int       // how difficult is this sudoku?
+	verbose    bool
 	//digitsFound []int     // how many of thee are in the matrix already
 }
 
@@ -17,37 +18,60 @@ type dimension int
 
 const (
 	Row   dimension = 0
-	Col             = 1
-	Block           = 2
+	Col   dimension = 1
+	Block dimension = 2
 )
 
-func (sudoku *Sudoku) Solve() (err error) {
-	for sudoku.missingTotal() > 0 {
-		if sudoku.findTrivial() {
-			fmt.Print(sudoku)
-			continue
-		}
-		return fmt.Errorf("Unsolveable, complexity=%d", sudoku.complexity)
-	}
-	return nil
+func (s *Sudoku) SetVerbose(verbose bool) {
+	s.verbose = verbose
 }
 
-func (sudoku *Sudoku) String() string {
-	s := ""
+func (s *Sudoku) Solve() bool {
+	step := 0
+	for !s.isDone() {
+		step++
+
+		if s.verbose {
+			fmt.Println("Step", step)
+		}
+
+		if s.findTrivial() {
+			if s.verbose {
+				fmt.Print(s)
+			}
+			continue
+		}
+		if s.findSimple() {
+			if s.verbose {
+				fmt.Print(s)
+			}
+			continue
+		}
+		return false
+	}
+	return true
+}
+
+func (s *Sudoku) String() string {
+	str := ""
 	for i := 0; i < 9; i++ {
 		if i%3 == 0 {
-			s += "+---+---+---+\n"
+			str += "+---+---+---+\n"
 		}
 		for j := 0; j < 9; j++ {
 			if j%3 == 0 {
-				s += "|"
+				str += "|"
 			}
-			s += fmt.Sprint(sudoku.matrix[i][j])
+			if s.matrix[i][j] == 0 {
+				str += " "
+			} else {
+				str += fmt.Sprint(s.matrix[i][j])
+			}
 		}
-		s += "|\n"
+		str += "|\n"
 	}
-	s += "+---+---+---+\n"
-	return s
+	str += "+---+---+---+\n"
+	return str
 }
 
 /*
@@ -57,29 +81,43 @@ Input can be:
   - comma separated digits where unknowns are X or 0 or empty
   - //a pretty-printed table
 */
-func (sudoku *Sudoku) Load(input []string) (err error) {
+func (s *Sudoku) Load(input string) (err error) {
 	i := 0
+	lineno := 0
+	lines := strings.Split(input, "\n")
 	for i < 9 {
-		line := input[i]
-		line = strings.Replace(line, ",", "", 9)
-		line = strings.Replace(line, "X", "0", 9)
+		line := lines[lineno]
+		line = strings.Replace(line, ",", "", -1)
+		line = strings.Replace(line, "-", "", -1)
+		line = strings.Replace(line, "+", "", -1)
+		line = strings.Replace(line, "|", "", -1)
+		line = strings.Replace(strings.ToLower(line), "x", "0", -1)
+		line = strings.Replace(line, " ", "0", -1)
+
+		if len(line) == 0 || line[0] == '#' || len(line) < 9 {
+			lineno++
+			continue
+		}
+
 		for j := 0; j < 9; j++ {
 			v := int(line[j]) - int('0')
 			if v < 0 || v > 9 {
-				return fmt.Errorf("Invalid input at %d %d", i, j)
+				return fmt.Errorf("invalid input at %d %d", i, j)
 			}
-			sudoku.matrix[i][j] = v
+			s.matrix[i][j] = v
 		}
+
+		lineno++
 		i++
 	}
 	return nil
 }
 
-func (sudoku *Sudoku) missingTotal() int {
+func (s *Sudoku) missingTotal() int {
 	n := 0
 	for i := 0; i < 9; i++ {
 		for j := 0; j < 9; j++ {
-			if sudoku.matrix[i][j] == 0 {
+			if s.matrix[i][j] == 0 {
 				n++
 			}
 		}
@@ -87,29 +125,31 @@ func (sudoku *Sudoku) missingTotal() int {
 	return n
 }
 
-func (sudoku *Sudoku) isDone() bool {
-	return sudoku.missingTotal() == 0
+func (s *Sudoku) isDone() bool {
+	return s.missingTotal() == 0
 }
 
 // 8 in a row, col or block => fill the missing one
-func (sudoku *Sudoku) findTrivial() bool {
+func (s *Sudoku) findTrivial() bool {
 	fillMissingItem := func(dim dimension, where int) bool {
 		n := 0
 		loc := 0
 		possiblevals := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 		for i := 0; i < 9; i++ {
 			r, c := translateIndextoRC(dim, where, i)
-			if sudoku.matrix[r][c] == 0 {
+			if s.matrix[r][c] == 0 {
 				n++
 				loc = i
 			} else {
-				possiblevals = removeFromSlice(possiblevals, sudoku.matrix[r][c])
+				possiblevals = removeFromSlice(possiblevals, s.matrix[r][c])
 			}
 		}
 		if n == 1 {
 			r, c := translateIndextoRC(dim, where, loc)
-			sudoku.matrix[r][c] = possiblevals[0]
-			fmt.Println("Filled", dim, "for index", where, "R:", r, "C:", c, "with ", possiblevals[0])
+			s.matrix[r][c] = possiblevals[0]
+			if s.verbose {
+				fmt.Println("Strategy 1", "R:", r, "C:", c, "V:", possiblevals[0])
+			}
 			return true
 		}
 		return false
@@ -119,16 +159,104 @@ func (sudoku *Sudoku) findTrivial() bool {
 		if fillMissingItem(Row, i) ||
 			fillMissingItem(Col, i) ||
 			fillMissingItem(Block, i) {
-			sudoku.setComplexity(1)
+			s.setComplexity(1)
 			return true
 		}
 	}
 	return false
 }
 
-func (sudoku *Sudoku) setComplexity(complexity int) {
-	if sudoku.complexity < complexity {
-		sudoku.complexity = complexity
+func (s *Sudoku) findSimple() bool {
+	// try to put v in all the free positions
+	for v := 1; v <= 9; v++ {
+		// OPT: if v has 9 instances, skip it
+		for r := 0; r < 9; r++ {
+			for c := 0; c < 9; c++ {
+				if s.matrix[r][c] == 0 &&
+					!s.hasAlready(Row, r, v) &&
+					!s.hasAlready(Col, c, v) &&
+					!s.hasAlready(Block, translateRCToBlock(r, c), v) {
+
+					r1 := 3*(r/3) + (r+1)%3
+					r2 := 3*(r/3) + (r+2)%3
+					c1 := 3*(c/3) + (c+1)%3
+					c2 := 3*(c/3) + (c+2)%3
+
+					r1has := s.hasAlready(Row, r1, v)
+					r2has := s.hasAlready(Row, r2, v)
+					c1has := s.hasAlready(Col, c1, v)
+					c2has := s.hasAlready(Col, c2, v)
+
+					// check if *other* rows/cols in the same row/col group have this v
+					// if yes for all => fill it in!
+					if r1has && r2has && c1has && c2has {
+						if s.verbose {
+							fmt.Println("Strategy 2a", "R:", r, "C:", c, "V:", v)
+						}
+						s.matrix[r][c] = v
+						s.setComplexity(2)
+						return true
+					}
+
+					// check if *other* rows/cols in the same row/col group have this v
+					// also, if *other* rows/cols cannot have it because in this block they are filled
+					// if yes for all => fill it in!
+					if (r1has || (s.matrix[r1][c] != 0 && s.matrix[r2][c] != 0)) &&
+						(r2has || (s.matrix[r1][c] != 0 && s.matrix[r2][c] != 0)) &&
+						(c1has || (s.matrix[r][c1] != 0 && s.matrix[r][c2] != 0)) &&
+						(c2has || (s.matrix[r][c1] != 0 && s.matrix[r][c2] != 0)) {
+						if s.verbose {
+							fmt.Println("Strategy 2b", "R:", r, "C:", c, "V:", v)
+						}
+						s.matrix[r][c] = v
+						s.setComplexity(2)
+						return true
+					}
+
+					// check if *other* rows/cols in the same row/col group have this v
+					// also, if *other* rows/cols cannot have it because in this block they are filled
+					// also, ignore already filled rows/cols
+					// if yes for all => fill it in!
+					if (r1has || (s.matrix[r1][c] != 0 && s.matrix[r1][c1] != 0 && s.matrix[r1][c2] != 0)) &&
+						(r2has || (s.matrix[r2][c] != 0 && s.matrix[r2][c1] != 0 && s.matrix[r2][c2] != 0)) &&
+						s.matrix[r][c1] != 0 && s.matrix[r][c2] != 0 {
+						if s.verbose {
+							fmt.Println("Strategy 2c1", "R:", r, "C:", c, "V:", v)
+						}
+						s.matrix[r][c] = v
+						s.setComplexity(2)
+						return true
+					}
+					if (c1has || (s.matrix[r][c1] != 0 && s.matrix[r1][c1] != 0 && s.matrix[r2][c1] != 0)) &&
+						(c2has || (s.matrix[r][c2] != 0 && s.matrix[r1][c2] != 0 && s.matrix[r2][c2] != 0)) &&
+						s.matrix[r1][c] != 0 && s.matrix[r2][c] != 0 {
+						if s.verbose {
+							fmt.Println("Strategy 2c2", "R:", r, "C:", c, "V:", v)
+						}
+						s.matrix[r][c] = v
+						s.setComplexity(2)
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
+}
+
+func (s *Sudoku) hasAlready(dim dimension, where int, val int) bool {
+	for i := 0; i < 9; i++ {
+		r, c := translateIndextoRC(dim, where, i)
+		if s.matrix[r][c] == val {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *Sudoku) setComplexity(complexity int) {
+	if s.complexity < complexity {
+		s.complexity = complexity
 	}
 }
 
@@ -153,4 +281,8 @@ func translateIndextoRC(dim dimension, where int, index int) (r, c int) {
 		return index/3 + 3*(where/3), index%3 + 3*(where%3)
 	}
 	return 9, 9 // this will blow up
+}
+
+func translateRCToBlock(r int, c int) int {
+	return (r/3)*3 + c/3
 }
---
+++ solver/solver_test.go
@@ -0,0 +1,264 @@
+package solver
+
+import (
+	"testing"
+)
+
+// Test complexity 1(a)
+func TestSolverLevel1a(t *testing.T) {
+
+	in := new(Sudoku)
+	err := in.Load(`
++---+---+---+
+|123|456|78X|
+|   |   |   |
+|   |   |   |
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+`)
+	if err != nil {
+		panic(err)
+	}
+	_ = in.Solve()
+
+	s := "\n" + in.String()
+	if s != `
++---+---+---+
+|123|456|789|
+|   |   |   |
+|   |   |   |
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+` {
+		t.Fatalf("incorrect soution for case 1a:\n%s", s)
+	}
+}
+
+func TestSolverLevel1b(t *testing.T) {
+
+	in := new(Sudoku)
+	err := in.Load(`
++---+---+---+
+|1  |   |   |
+|2  |   |   |
+|3  |   |   |
++---+---+---+
+|4  |   |   |
+|5  |   |   |
+|6  |   |   |
++---+---+---+
+|7  |   |   |
+|8  |   |   |
+|X  |   |   |
++---+---+---+
+`)
+	if err != nil {
+		panic(err)
+	}
+	_ = in.Solve()
+
+	s := "\n" + in.String()
+	if s != `
++---+---+---+
+|1  |   |   |
+|2  |   |   |
+|3  |   |   |
++---+---+---+
+|4  |   |   |
+|5  |   |   |
+|6  |   |   |
++---+---+---+
+|7  |   |   |
+|8  |   |   |
+|9  |   |   |
++---+---+---+
+` {
+		t.Fatalf("incorrect soution for case 1b:\n%s", s)
+	}
+}
+
+func TestSolverLevel1c(t *testing.T) {
+
+	in := new(Sudoku)
+	err := in.Load(`
++---+---+---+
+|123|   |   |
+|456|   |   |
+|78X|   |   |
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+`)
+	if err != nil {
+		panic(err)
+	}
+	_ = in.Solve()
+
+	s := "\n" + in.String()
+	if s != `
++---+---+---+
+|123|   |   |
+|456|   |   |
+|789|   |   |
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+` {
+		t.Fatalf("incorrect soution for case 1c:\n%s", s)
+	}
+}
+
+func TestSolverLevel2a(t *testing.T) {
+
+	in := new(Sudoku)
+	err := in.Load(`
++---+---+---+
+|1  |   |   |
+|   |1  |   |
+|   |   |  X|
++---+---+---+
+|   |   |1  |
+|   |   |   |
+|   |   |   |
++---+---+---+
+|   |   | 1 |
+|   |   |   |
+|   |   |   |
++---+---+---+
+`)
+	if err != nil {
+		panic(err)
+	}
+	_ = in.Solve()
+
+	s := "\n" + in.String()
+	if s != `
++---+---+---+
+|1  |   |   |
+|   |1  |   |
+|   |   |  1|
++---+---+---+
+|   |   |1  |
+|   |   |   |
+|   |   |   |
++---+---+---+
+|   |   | 1 |
+|   |   |   |
+|   |   |   |
++---+---+---+
+` {
+		t.Fatalf("incorrect soution for case 2a:\n%s", s)
+	}
+}
+
+func TestSolverLevel2b(t *testing.T) {
+
+	in := new(Sudoku)
+	err := in.Load(`
++---+---+---+
+|1  |   |   |
+|   |1  |   |
+|   |   |23X|
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+`)
+	if err != nil {
+		panic(err)
+	}
+	_ = in.Solve()
+
+	s := "\n" + in.String()
+	if s != `
++---+---+---+
+|1  |   |   |
+|   |1  |   |
+|   |   |231|
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+` {
+		t.Fatalf("incorrect soution for case 2b:\n%s", s)
+	}
+}
+
+func TestSolverLevel2c(t *testing.T) {
+
+	in := new(Sudoku)
+	err := in.Load(`
++---+---+---+
+|1  |   |   |
+|   |   |456|
+|   |   |23X|
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+`)
+	if err != nil {
+		panic(err)
+	}
+	_ = in.Solve()
+
+	s := "\n" + in.String()
+	if s != `
++---+---+---+
+|1  |   |   |
+|   |   |456|
+|   |   |231|
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+|   |   |   |
+|   |   |   |
+|   |   |   |
++---+---+---+
+` {
+		t.Fatalf("incorrect soution for case 2c:\n%s", s)
+	}
+}
